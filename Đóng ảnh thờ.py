c21f5c612c1a8286415a70ced33165caff7fc428
hey, where's my delivery !? <88236617+Du4nG@users.noreply.github.com>
2022-05-21 14:31:38 +0700

    Update Đóng ảnh thờ.py

--- Đóng ảnh thờ.py
+++ Đóng ảnh thờ.py
@@ -3,11 +3,11 @@ vid = cv2.VideoCapture('tự_quay_video_vài_giây_rồi_bỏ_vào.mp4')
 success,image = vid.read()
 count = 0
 while success:
-    img = image[200:500,500:800] # canh mặt ở đâu mà đóng khung vào nhé
+    img = image[200:500,500:800] # canh mặt ở đâu mà đóng khung vào nhé.
     img = cv2.resize(img,(150,150),fx=0,fy=0,interpolation=cv2.INTER_CUBIC)
     cv2.imwrite("frame%d.jpg" % count, img)      
     success, img = vidcap.read()
     print('Read a new frame: ', success)
     count += 1
     
-# Sau đó đưa đường dẫn qua file dưachuột.py để chạy mà xuất file nhé.
+# Nhét hết hình vào folder cuối cùng của path trong dưachuột.py thoi
