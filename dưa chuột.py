0284700e2cf53f314520a4d9431e3d8c6ab52b33
hey, where's my delivery !? <88236617+Du4nG@users.noreply.github.com>
2022-05-21 14:29:53 +0700

    Update dưa chuột.py

--- dưa chuột.py
+++ dưa chuột.py
@@ -1,4 +1,4 @@
-#Dán nhãn cho mấy thằng vô danh tiểu tốt nè
+# Dán nhãn cho mấy thằng vô danh tiểu tốt nào
 
 import cv2
 import glob
@@ -7,20 +7,20 @@ import pickle
 x_train = []
 y_train = []
 
-for filename in glob.glob("D:\\X\\Code\\Python\\AI\\data\\0\\*.jpg"): #cop mấy trăm hình thờ vô
+for filename in glob.glob("D:\\X\\Code\\Python\\AI\\data\\0\\*.jpg"): # Cop mấy trăm hình thờ vô
     img = cv2.resize(cv2.imread(filename), (150,150))
     x_train.append(img)
     y_train.append(0)
 
-for filename in glob.glob("D:\\X\\Code\\Python\\AI\\data\\1\\*.jpg"):
-    img = cv2.resize(cv2.imread(filename), (150,150))
-    x_train.append(img)
-    y_train.append(1)
+''' for filename in glob.glob("D:\\X\\Code\\Python\\AI\\data\\1\\*.jpg"):
+        img = cv2.resize(cv2.imread(filename), (150,150))
+        x_train.append(img)
+        y_train.append(1)
 
-for filename in glob.glob("D:\\X\\Code\\Python\\AI\\data\\2\\*.jpg"):
-    img = cv2.resize(cv2.imread(filename), (150,150))
-    x_train.append(img)
-    y_train.append(2)
+    for filename in glob.glob("D:\\X\\Code\\Python\\AI\\data\\2\\*.jpg"):
+        img = cv2.resize(cv2.imread(filename), (150,150))
+        x_train.append(img)
+        y_train.append(2)'''
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
